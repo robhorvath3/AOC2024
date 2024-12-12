e72e26404fbd73f22b9334c0407aa0c2339cf964
= <=>
2024-12-11 22:38:30 -0500

    Finished Day 11

---
+++ Day11-1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Day11_1
+{
+    internal class Program
+    {
+        internal class Stone
+        {
+            public Int64 Engraving;
+
+            public Stone(Int64 engraving)
+            {
+                this.Engraving = engraving;
+            }
+
+            public Stone? Blink()
+            {
+                if (this.Engraving == 0)
+                {
+                    this.Engraving = 1;
+                    return null;
+                }
+                else if (this.Engraving.ToString().Trim().Length % 2 != 0)
+                {
+                    this.Engraving *= 2024;
+                    return null;
+                }
+                else
+                {
+                    StringBuilder sb_left = new StringBuilder();
+                    StringBuilder sb_right = new StringBuilder();
+                    string eng = this.Engraving.ToString().Trim();
+                    int eng_len = eng.Length;
+
+                    for (int i = 0; i < eng_len; i++)
+                    {
+                        if (i < eng_len / 2)
+                        {
+                            sb_left.Append(eng[i]);
+                        }
+                        else
+                        {
+                            sb_right.Append(eng[i]);
+                        }
+                    }
+
+                    this.Engraving = Int64.Parse(sb_left.ToString());
+                    return new Stone(Int64.Parse(sb_right.ToString()));
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            string input;
+            LinkedList<Stone> stone_list = new LinkedList<Stone>();
+
+            using (StreamReader sr = new StreamReader("Input.txt"))
+            {
+                try
+                {
+                    input = sr.ReadToEnd();
+
+                    Int64[] stone_numbers = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => Int64.Parse(x)).ToArray();
+
+                    for (int i = 0; i < stone_numbers.Length; i++)
+                    {
+                        stone_list.AddLast(new Stone(stone_numbers[i]));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception: {ex.Message}");
+                }
+                finally
+                {
+                    sr.Close();
+                }
+            }
+
+            for (int blink_count = 0; blink_count < 25; blink_count++)
+            {
+                Dictionary<Stone, Stone> new_stones = new Dictionary<Stone, Stone>();
+
+                foreach (Stone stone in stone_list)
+                {
+                    Stone? new_stone = stone.Blink();
+
+                    if (new_stone != null)
+                    {
+                        new_stones.Add(stone, new_stone);
+                    }
+                }
+
+                foreach (Stone source_stone in new_stones.Keys)
+                {
+                    LinkedListNode<Stone> new_stone_node = new LinkedListNode<Stone>(new_stones[source_stone]);
+                    stone_list.AddAfter(stone_list.Find(source_stone), new_stone_node);
+                }
+            }
+
+            Console.WriteLine($"After blinking 25 times, there are now {stone_list.Count} stones");
+        }
+    }
+}
---
+++ Day11-2/Program.cs
@@ -0,0 +1,135 @@
+﻿using System.IO;
+using System.Text;
+using System.Collections.Generic;
+
+namespace Day11_2
+{
+    // no brute force today
+    // flip the problem
+    internal class Program
+    {
+        internal struct Pair
+        {
+            public long X;
+            public long Y;
+
+            public Pair(long x, long y)
+            {
+                X = x;
+                Y = y;
+            }
+
+            public void Set(long x, long y)
+            {
+                X = x;
+                Y = y;
+            }
+        }
+
+        internal static Dictionary<long, Pair> pair_cache = new Dictionary<long, Pair>();
+        static Pair Blink(long engraving)
+        {
+            if (pair_cache.ContainsKey(engraving))
+                return pair_cache[engraving];
+
+            Pair pair = new Pair();
+
+            if (engraving == 0)
+            {
+                pair.Set(1, -1);
+            }
+            else
+            {
+                string eng = engraving.ToString();
+
+                if (eng.Length % 2 == 0)
+                {
+                    pair.Set(long.Parse(eng.Substring(0, eng.Length / 2)), long.Parse(eng.Substring(eng.Length / 2, eng.Length / 2)));
+                }
+                else
+                {
+                    pair.Set(engraving * 2024, -1);
+                }                
+            }
+
+            pair_cache.Add(engraving, pair);
+            return pair;
+        }
+
+        internal static Dictionary<long, long> stone_map;
+        internal static Dictionary<long, long> stone_updates = new Dictionary<long, long>();
+
+        internal static void ProcessStones(long k)
+        {
+            Pair pair = Blink(k);
+
+            if (stone_updates.ContainsKey(pair.X))
+            {
+                stone_updates[pair.X] += stone_map[k];
+            }
+            else
+            {
+                stone_updates.Add(pair.X, stone_map[k]);
+            }
+
+            if (pair.Y != -1)
+            {
+                if (stone_updates.ContainsKey(pair.Y))
+                {
+                    stone_updates[pair.Y] += stone_map[k];
+                }
+                else
+                {
+                    stone_updates.Add(pair.Y, stone_map[k]);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string input;
+
+            using (StreamReader sr = new StreamReader("Input.txt"))
+            {
+                try
+                {
+                    input = sr.ReadToEnd();
+                    stone_map = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => Int64.Parse(x)).ToDictionary(s => s, s => (long)1);
+                    
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Exception: {ex.Message}");
+                }
+                finally
+                {
+                    sr.Close();
+                }
+            }
+            
+            int blink_count = 0;
+
+            do
+            {
+                foreach (long k in stone_map.Keys)
+                {
+                    if (stone_map[k] > 0)
+                        ProcessStones(k);
+                }
+
+                stone_map = stone_updates.ToDictionary();
+                stone_updates.Clear();
+                blink_count++;
+
+            } while (blink_count < 75);
+
+            long stone_count = 0;
+            foreach (long k in stone_map.Keys)
+            {
+                stone_count += stone_map[k];
+            }
+
+            Console.WriteLine($"After blinking 75 times, there are now {stone_count} stones");
+        }
+    }
+}
